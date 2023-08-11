f236720b738eb7271cba76da2c3f7de270c1d566
ygor-calimanis <ygoralvescalimans@hotmail.com>
2023-08-11 12:43:03 -0300

    first commit

---
+++ fundamentos/type-number.js
@@ -0,0 +1,26 @@
+// tipo Number
+
+const myNumber = 3;
+
+const firstNumber = 1;
+
+const secondNumber = 2;
+
+const mathOperation = firstNumber * secondNumber;
+
+console.log(mathOperation)
+
+// ponto flutuante
+
+const floatNumber = 3.3;
+const floatNumberWhitoutZero = .5;
+
+const newOperation = firstNumber * floatNumber;
+
+console.log(newOperation);
+
+// NaN -> Not a Number
+
+const lorem = "Lorem";
+
+console.log(lorem*firstNumber);
---
+++ fundamentos/type-string.js
@@ -0,0 +1,17 @@
+// type STRING
+
+const text1 = "Hello World!";
+const text2 = 'Hello World!';
+const password = 'superSecretPassword123456!';
+const NumberString = '1234567890';
+
+const citation = 'My name is:';
+
+// concatenação (+)
+const myName = 'Ygor';
+const concatenation = citation + myName;
+console.log(concatenation)
+
+
+// template string
+
